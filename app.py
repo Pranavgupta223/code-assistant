3be7977506ad7bc0b41ddfe3c98299ba2eb4e547
Pranav <guptapranav5128@gmail.com>
2026-04-15 16:29:09 +0530

    fix ask endpoint response

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
+=from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
+
+from retriever import search_query
+from llm import generate_answer
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
 
 class QueryRequest(BaseModel):
     query: str
+
+
+@app.get("/ui")
+def ui(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html"
+    )
+
 
 @app.post("/ask")
 def ask(request: QueryRequest):
